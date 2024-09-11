5491bf92a9c1b3474e0d8b5c95f893e1bd82bb6c
cgaleam <carlos28galea@gmail.com>
2024-09-11 12:39:31 +0200

    New commit

---
+++ online-shop/src/App.jsx
@@ -0,0 +1,12 @@
+import { Products } from "./Components/Products.jsx"
+import { products } from "./mocks/products.json"
+function App() {
+
+  return (
+
+    <Products products={products}/>
+
+  )
+}
+
+export default App
---
+++ online-shop/src/Components/Products.jsx
@@ -0,0 +1,23 @@
+//Componente frontend para mostrar la lista de productos y la interación con ellos.
+
+import './Products.css'
+import { AddToCartIcon } from './Icons'
+
+export const Products = ({products}) => {
+    return ( 
+        <main className="products">
+            <ul>
+                {
+                    products.map(product => <li key={product.id}><img src={product.thumbnail} alt={product.title}/>
+                    <div>
+                        <strong>{product.title}</strong>-${product.price}
+                    </div>
+                    <div>
+                        <button><AddToCartIcon/></button>
+                    </div>
+                
+                </li>) }
+            </ul>
+        </main>
+    )
+}
