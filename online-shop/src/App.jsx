b3f9a6dc2faf94f627566b2a9a6d6771038c5084
cgaleam <carlos28galea@gmail.com>
2024-09-19 14:12:51 +0200

    Commit sincronizacion

---
+++ online-shop/src/App.jsx
@@ -0,0 +1,46 @@
+//Componente frontend para mostrar la lista de productos y la interación con ellos.
+import {products as initialProducts} from "./mocks/products.json"
+import { Products } from "./Components/Products.jsx"
+import { useState } from "react"
+import { Header } from "./Components/Header.jsx"
+
+
+function useFilters (){   //hub que se encarga de los filtross
+  //inicializo los filtros
+  const[filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  //funcion para filtrar los productos
+  const filterProducts= (products) => {
+    return products.filter(product => {
+      return (product.price>= filters.minPrice && 
+        (filters.category === 'all' || product.category === filters.category))
+    })
+  }
+
+  return { filterProducts, setFilters}
+}
+
+
+function App() {
+
+  //Inicializo los productos
+  const[products]= useState(initialProducts)
+
+  const {filterProducts, setFilters} = useFilters()
+
+  //aplico los filtros a los productos
+  const filteredProducts= filterProducts(products)  
+
+  //muestro los datos
+  return (
+    <>
+    <Header changeFilters={setFilters}/>
+    <Products products={filteredProducts}/>
+    </>
+  )
+}
+
+export default App
